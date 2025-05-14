828ac249fb459ce0cf70460371da0aa7ece65b0a
ayush <rajput.ayush8jn@gmail.com>
2025-05-14 12:36:41 +0530

    Add contributor section to footer; include links for Ayush and Somitav with respective social media profiles

--- frontend/src/libs/constants.ts
+++ frontend/src/libs/constants.ts
@@ -7,8 +7,14 @@ export const TWITTER_URL = 'https://x.com/ayushrajput8021';
 export const UPWORK_URL =
 	'https://www.upwork.com/freelancers/~01b7929bb5df9624dc';
 
+// Somitav's links
+export const SOMITAV_GITHUB_URL = 'https://github.com/Somitav';
+export const SOMITAV_LINKEDIN_URL =
+	'https://www.linkedin.com/in/somitav-goswami-778113170';
+export const SOMITAV_GMAIL_URL = 'somitavgoswami@gmail.com';
+
 // App-specific constants
 export const APP_NAME = 'Spam Email Detector';
-export const FOOTER_TEXT = 'Crafted with ❤️ by Ayush';
+export const FOOTER_TEXT = 'Crafted with ❤️ by Ayush & Somitav';
 export const CURRENT_YEAR = new Date().getFullYear();
 export const COPYRIGHT_TEXT = `© ${CURRENT_YEAR} ${APP_NAME}`;
